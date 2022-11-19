a4989884d3682b21f2c85ff11827de2f463eeef6
Pavel <impetus@yandex.ru>
2022-11-19 13:13:12 +0300

    Add textChangeListener

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.kt
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.kt
@@ -3,52 +3,40 @@ package ru.samsung.itschool.mdev.myapplication
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var counter = 0
+    private lateinit var editText: EditText
+    private lateinit var tv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.editText)
+        tv = findViewById(R.id.textView)
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
-        Toast.makeText(this,resources.getText(R.string.ncreate), Toast.LENGTH_LONG).show()
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if(p0.toString().isNotEmpty()) {
+                    val kg = p0.toString().toInt()
+                    val gramm = kg * 1000
+                    tv.text = "$gramm gramm"
+                }
+            }
+        })
+
+       // Toast.makeText(this,resources.getText(R.string.ncreate), Toast.LENGTH_LONG).show()
     }
 
-    override fun onRestart() {
-        super.onRestart()
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        counter++
-        if(counter%2 == 1) {
-
-        }
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-    }
 }
